add3e92f61e040f16e4bd9739c356dee344dbf53
Kurtis <kurtisyim27@gmail.com>
2026-03-04 07:25:18 +0000

    updated scripts

--- scripts/vcf_to_ped.sh
+++ scripts/vcf_to_ped.sh
@@ -10,6 +10,8 @@ for chr in {1..22}; do
         --snps-only just-acgt \
         --max-alleles 2 \
         --min-alleles 2 \
+        --geno 0 \
+        --mind 0 \
         --allow-extra-chr
 
 done
