3bd03f1792a7c05fe2543e5ba90761907721ac13
Kurtis <kurtisyim27@gmail.com>
2026-03-05 04:50:03 +0000

    beagle IBD analysis

--- scripts/beagle_ibd.sh
+++ scripts/beagle_ibd.sh
@@ -2,9 +2,9 @@
 
 for chr in {1..22}; do
   java -jar ~/Project/beagle/beagle.27Jan18.7e1.jar \
-    gt=../data/ps2_ibd_phased.vcf.gz \
+    gt=data/ps2_ibd_phased.vcf.gz \
     chrom=$chr \
-    map=../data/maps/chr${chr}.map \
-    out=../data/beagle/ps2_ibd_chr${chr} \
+    map=data/maps/chr${chr}.map \
+    out=data/beagle/ps2_ibd_chr${chr} \
     ibd=true
 done
